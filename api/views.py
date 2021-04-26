9175b0389877b26f183ebe83167b326a89cd183a
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-04-26 15:15:05 -0400

    update index view to match new model

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Report, TimeseriesCases, TimeseriesVaccination
+from .models import Report
 from django.db.models import Prefetch
 from django.forms.models import model_to_dict
 from pprint import pprint
@@ -10,49 +10,23 @@ import time
 
 def index(request):
     start = time.process_time()
-    
-    # Retrieve all rows of timeseries_cases and timeseries_vaccination
-    report = Report.objects.prefetch_related(
-        Prefetch('timeseries_cases', to_attr='ts_cases'),
-        Prefetch('timeseries_vaccination', to_attr='ts_vacci'),
-        Prefetch('variants', to_attr='ts_variants'),
-    )
 
+    # Retrieve all records
+    fields = [i.name for i in Report._meta.get_fields()][2:]
+    ret_field_names = [
+        'Date String', 'New Cases', 'New Deaths', 'New Tests', 'Total Cases', 
+        'Total Deaths', 'Total Resolved', 'Total Active', 'New Vaccinations',
+        'Num Fully Vaccinated', 'Num Part Vaccinated', 'P1 (Brazil)', 
+        'B1351 (South Africa)', 'B117 (UK)',
+    ]
     res = {}
-    res['data'] = []
-    res['ts_cases_start'] = -1
-    res['ts_vacci_start'] = -1
-    res['ts_variant_start'] = -1
-
-    for count, i in enumerate(report):
-        to_append = model_to_dict(i, exclude=['date'])
-        if i.ts_cases:
-            ts_cases_report = model_to_dict(i.ts_cases[0], exclude=['report'])
-            to_append['ts_cases'] = ts_cases_report
-
-            if res['ts_cases_start'] == -1:
-                res['ts_cases_start'] = count
-        
-        if i.ts_vacci:
-            ts_vacci_report = model_to_dict(i.ts_vacci[0], exclude=['report'])
-            to_append['ts_vacci'] = ts_vacci_report
-
-            if res['ts_vacci_start'] == -1:
-                res['ts_vacci_start'] = count
-        
-        if i.ts_variants[0].b117 != 0:
-            ts_variant_report = model_to_dict(i.ts_variants[0], exclude=['report'])
-            to_append['ts_variant'] = ts_variant_report
-
-            if res['ts_variant_start'] == -1:
-                res['ts_variant_start'] = count
-        
-        res['data'].append(to_append)
+    res['data'] = [ret_field_names] + list(Report.objects.values_list(*fields))
     
     end = time.process_time() - start
     print(end)
 
     response = HttpResponse(json.dumps(res), content_type="application/json")
     response['Access-Control-Allow-Origin'] = '*'
+    # response['Cache-Control'] = 'max-age=1800'
     response['Server-Timing'] = f'db;desc="Database";dur={end}'
     return response
